7e5b6e88353ee62fd07961f334cb7aa51ea1b3dc
Denis Scherbatykh <shdv09@gmail.com>
2019-12-11 17:37:15 +0300

    testing Bean scopes

--- src/main/Java/ru/alishev/springcourse/TestSpring.java
+++ src/main/Java/ru/alishev/springcourse/TestSpring.java
@@ -5,10 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
-        player.playSong();
-        System.out.println(player.getName());
-        System.out.println(player.getVolume());
+        MusicPlayer firstPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(firstPlayer == secondPlayer);
+        System.out.println(firstPlayer);
+        System.out.println(secondPlayer);
+
+        secondPlayer.setVolume(20);
+
+        firstPlayer.playSong();
+        System.out.println(firstPlayer.getName());
+        System.out.println(firstPlayer.getVolume());
         context.close();
     }
 }
